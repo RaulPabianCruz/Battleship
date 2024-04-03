ec595d1f9d369c60fac818e7874c4522afc45ee1
RaulPabianCruz <ito.pabian@gmail.com>
2024-04-02 20:19:24 -0700

    Move layout-generating-functions to their own module

---
+++ src/layout.js
@@ -0,0 +1,48 @@
+function initializeHeader() {
+  let header = document.querySelector('.header');
+  let h2 = document.createElement('h2');
+  h2.classList.add('header-title');
+  h2.textContent = 'Battleship, The Game';
+  header.append(h2);
+}
+
+function initializeStatus() {
+  let status = document.querySelector('.status-container');
+  let h3 = document.createElement('h3');
+  h3.classList.add('status-text');
+  status.append(h3);
+}
+
+function intializeGridContent() {
+  let gridContent = document.querySelector('.grid-content');
+  let playerSection = createSection('player');
+  let opponentSection = createSection('opponent');
+  gridContent.appendChild(playerSection);
+  gridContent.appendChild(opponentSection);
+}
+
+function createSection(playerString) {
+  let section = document.createElement('div');
+  section.classList.add(`${playerString}`);
+  section.classList.add('grid-section-container');
+
+  let sectionTitle = document.createElement('h4');
+  sectionTitle.classList.add(`${playerString}`);
+  sectionTitle.classList.add('grid-title');
+
+  let sectionGrid = document.createElement('div');
+  sectionGrid.classList.add(`${playerString}`);
+  sectionGrid.classList.add('grid');
+
+  section.appendChild(sectionTitle);
+  section.appendChild(sectionGrid);
+  return section;
+}
+
+function initializeLayout() {
+  initializeHeader();
+  initializeStatus();
+  intializeGridContent();
+}
+
+export default initializeLayout;
