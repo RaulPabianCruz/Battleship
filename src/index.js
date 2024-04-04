83f2778b4a4e73b29170a508449b8101c297cc81
RaulPabianCruz <ito.pabian@gmail.com>
2024-04-03 18:11:46 -0700

    Add index.js to use and display the battleship game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import './style.css';
+import initializeLayout from './layout';
+import DOMController from './DomInteraction';
 
+initializeLayout();
+
+let controller = DOMController();
+controller.resetGameDisplay();
+
+let resetBttn = document.querySelector('.reset-bttn');
+resetBttn.addEventListener('click', controller.resetGameDisplay);
